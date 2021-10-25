3b7e27705b0e99afc6fca55818027f42cdabbe24
Olya Severova <34347298+olyaseverova@users.noreply.github.com>
2021-10-25 22:27:44 +0300

    vodoo test done

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,75 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main">
+    <input
+      class="main__input"
+      type="text"
+      placeholder="Filter by author..."
+      @input="handleInput"
+    />
+    <ul class="posts-list list">
+      <li
+        class="posts-list__item"
+        v-for="post in posts"
+        :key="post.id"
+        :id="post.userId"
+      >
+        <h3 class="posts-list__headline">{{ post.title }}</h3>
+        <p class="posts-list__description">{{ post.body }}</p>
+        <ul class="users-list list ul">
+          <li
+            class="users-list__item none"
+            v-for="user in users"
+            :key="user.id"
+            :class="{ block: user.id === post.userId }"
+          >
+            <p class="users-list__author" :id="post.userId">{{ user.name }}</p>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  data() {
+    return {
+      posts: null,
+      users: null,
+      message: "",
+    };
+  },
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  methods: {
+    handleInput: function (event) {
+      let posts = document.querySelectorAll(".posts-list__item");
+      let users = document.querySelectorAll(".users-list__item");
+      let patternRegExp = new RegExp(event.target.value);
+
+      posts.forEach(function (post) {
+        post.classList.add("none");
+        users.forEach(function (user) {
+          if (user.classList.contains("block")) {
+            let author = user.querySelector(".users-list__author");
+            if (patternRegExp.test(author.innerText) && author.id === post.id) {
+              post.classList.remove("none");
+            }
+          }
+        });
+      });
+    },
+  },
+
+  mounted() {
+    axios
+      .get("http://jsonplaceholder.typicode.com/posts")
+      .then((response) => (this.posts = response.data));
+
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => (this.users = response.data));
+  },
+};
+</script>
